8c9eb2d2cef1fedbd014dfcf2fb470a973400bfd
Austin <198095388+constAustiiiCodes@users.noreply.github.com>
2025-07-22 10:22:20 +0200

    Create playRound function that takes the human and computer choices to     play a single round then increments the round winner’s score and logs a winner announcement.

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let computerScore = 0;
 
 function getHumanChoice() {
     const humanChoice = prompt();
+    console.log(`You chose: ${humanChoice}`);
     return humanChoice;
 }
-console.log(getHumanChoice());
 
 function getComputerChoice() {
     let computerChoice = Math.random() * 3;
@@ -16,6 +16,37 @@ function getComputerChoice() {
     } else {
         computerChoice = "scissors";
     }
-    return computerChoice
+    console.log(`Computer chose: ${computerChoice}`);
+    return computerChoice;
 }
-console.log(getComputerChoice());
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("It's a draw! Play again!");
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore++
+        console.log("You win! Rock beats scissors.");
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++
+        console.log("You win! Scissors beats paper.")
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore++
+        console.log("You win! Paper beats rock.")
+    } else if (computerChoice == "rock" && humanChoice == "scissors") {
+        computerScore++
+        console.log("You lose! Rock beats scissors.");
+    } else if (computerChoice == "scissors" && humanChoice == "paper") {
+        computerScore++
+        console.log("You lose! Scissors beats paper.")
+    } else if (computerChoice == "paper" && humanChoice == "rock") {
+        computerScore++
+        console.log("You lose! Paper beats rock.");
+    } 
+    console.log(humanScore);
+    console.log(computerScore);
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
